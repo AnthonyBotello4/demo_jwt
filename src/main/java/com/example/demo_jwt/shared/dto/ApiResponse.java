ad9465e0d491b8843166e053d8ba83724aa45936
Anthony Botello <89035933+AnthonyBotello4@users.noreply.github.com>
2024-10-10 01:20:00 -0500

    feat: shared

---
+++ src/main/java/com/example/demo_jwt/shared/dto/ApiResponse.java
@@ -0,0 +1,20 @@
+package com.example.demo_jwt.shared.dto;
+
+import lombok.Data;
+
+@Data
+public class ApiResponse<T> {
+
+    private boolean success;
+    private String message;
+    private T data;
+
+    public ApiResponse() {
+    }
+
+    public ApiResponse(boolean success, String message, T data) {
+        this.success = success;
+        this.message = message;
+        this.data = data;
+    }
+}
