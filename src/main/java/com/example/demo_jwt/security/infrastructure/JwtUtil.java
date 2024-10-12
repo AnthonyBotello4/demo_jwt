0f8580bca15eaf6e0d042cededa282e22826b84c
Anthony Botello <89035933+AnthonyBotello4@users.noreply.github.com>
2024-10-12 12:35:56 -0500

    fix: jwt configuration

--- src/main/java/com/example/demo_jwt/security/infrastructure/JwtUtil.java
+++ src/main/java/com/example/demo_jwt/security/infrastructure/JwtUtil.java
@@ -3,9 +3,12 @@ package com.example.demo_jwt.security.infrastructure;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -14,17 +17,11 @@ import java.util.function.Function;
 @Component
 public class JwtUtil {
 
-    private final SecretKey secretKey =  Jwts.SIG.HS256.key().build();
+    private final SecretKey secretKey;
 
-            //new SecretKeySpec("secret".getBytes(StandardCharsets.UTF_8), "HmacSHA256");
-
-    /*public JwtUtil(@Value("${jwt.secret}") String secret) {
-        this.secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), "HmacSHA256");
+    public JwtUtil(@Value("${jwt.secret}") String secret) {
+        this.secretKey = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
     }
-
-    public SecretKey getSecretKey() {
-        return secretKey;
-    }*/
 
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
